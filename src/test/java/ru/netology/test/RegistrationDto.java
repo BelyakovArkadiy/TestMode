f5620895726ad08f4052ec09b09463df54921ae3
BelyakovArkadiy <belyakovarkadiy@gmail.com>
2021-03-12 15:47:39 +0600

    Initial commit

---
+++ src/test/java/ru/netology/test/RegistrationDto.java
@@ -0,0 +1,9 @@
+package ru.netology.test;
+
+import java.io.File;
+
+public class RegistrationDto extends File {
+    public RegistrationDto(String vasya, String password, String active) {
+        super();
+    }
+}
