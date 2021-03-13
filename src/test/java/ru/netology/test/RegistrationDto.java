0f80008d16846d60538b435637dc5446fee8fcce
BelyakovArkadiy <belyakovarkadiy@gmail.com>
2021-03-13 18:41:28 +0600

    Changes to be committed:

--- src/test/java/ru/netology/test/RegistrationDto.java
+++ src/test/java/ru/netology/test/RegistrationDto.java
@@ -1,17 +1,39 @@
 package ru.netology.test;
 
+
+import com.github.javafaker.Faker;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Value;
+
+import java.util.Locale;
+import java.util.Random;
 
 @Data
-@NoArgsConstructor
+@Value
 @AllArgsConstructor
-
-
-
+@NoArgsConstructor
 public class RegistrationDto {
-    public RegistrationDto(String user, String password, String status) {
-        return ;
+
+
+    Faker faker = new Faker(new Locale("en"));
+
+
+
+    public static String newLogin(Faker faker) {
+        return faker.name().firstName();
     }
+    public static String newPassword(Faker faker){
+        return faker.internet().password();
+    }
+    public static String getStatus(){
+        String[] status = {"active","blocked"};
+        int currentStatus = new Random().nextInt(status.length);
+        return status[currentStatus];
+    }
+
 }
+
+
+
--- src/test/java/ru/netology/test/TestModeTest.java
+++ src/test/java/ru/netology/test/TestModeTest.java
@@ -1,5 +1,7 @@
 package ru.netology.test;
 
+import com.github.javafaker.Faker;
+import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
@@ -7,12 +9,18 @@ import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+import java.util.Locale;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.visible;
 
-
-import static io.restassured.RestAssured.given;
 
 public class TestModeTest {
+    RegistrationDto user = new RegistrationDto();
+    Faker faker = new Faker(Locale.ENGLISH);
     private static RequestSpecification requestSpec = new RequestSpecBuilder()
             .setBaseUri("http://localhost")
             .setPort(9999)
@@ -23,20 +31,29 @@ public class TestModeTest {
 
     @BeforeAll
     static void setUpAll() {
+        RestAssured.given()
+                .spec(requestSpec) // указываем, какую спецификацию используем
+                .body(new RegistrationDto()) // передаём в теле объект, который будет преобразован в JSON
+                .when() // "когда"
+                .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
+                .then() // "тогда ожидаем"
+                .statusCode(200); // код 200 OK
+    }
 
-            open("http://localhost:9999");
-        }
-        @Test
-        void shouldTestAllFields () {
-            // сам запрос
-            given() // "дано"
-                    .spec(requestSpec) // указываем, какую спецификацию используем
-                    .body(new RegistrationDto("vasya", "password", "active")) // передаём в теле объект, который будет преобразован в JSON
-                    .when() // "когда"
-                    .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
-                    .then() // "тогда ожидаем"
-                    .statusCode(200); // код 200 OK
-        }
+    @Test
+    void shouldValidUser() {
+        open("http://localhost:9999");
+        $("[type='text']").setValue(user.newLogin(faker));
+        $("[type='password']").setValue(user.newPassword(faker));
+        $(".button__text").click();
+        $(withText("Личный кабинет")).shouldBe(visible, Duration.ofSeconds(15));
+
 
     }
+
+}
+
+
+
+
 
